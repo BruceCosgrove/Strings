a1348359a1e800a6acb8784b451a0a07dbd7ea22
Shlayne <1901.brc@gmail.com>
2024-09-06 16:53:25 -0700

    Implement string assignment const pointer tests.

---
+++ test/src/StringAssignmentOperator_ConstPointer_Test.cpp
@@ -0,0 +1,71 @@
+#include "common.hpp"
+
+TEST(StringAssignmentOperator_ConstPointer, EmptyString_AssignedWith_EmptyStringView) {
+    string s1;
+    s1 = Empty.data();
+
+    AssertEmpty(s1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, EmptyString_AssignedWith_SmallStringView) {
+    string s1;
+    s1 = Small1.data();
+
+    AssertSmall(s1, Small1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, EmptyString_AssignedWith_LargeStringView) {
+    string s1;
+    s1 = Large1.data();
+
+    AssertLarge(s1, Large1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, SmallString_AssignedWith_EmptyStringView) {
+    string s1(Small1);
+    s1 = Empty.data();
+
+    AssertEmpty(s1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, SmallString_AssignedWith_SmallStringView) {
+    string s1(Small1);
+    s1 = Small2.data();
+
+    AssertSmall(s1, Small2);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, SmallString_AssignedWith_LargeStringView) {
+    string s1(Small1);
+    s1 = Large1.data();
+
+    AssertLarge(s1, Large1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, LargeString_AssignedWith_EmptyStringView) {
+    string s1(Large1);
+    s1 = Empty.data();
+
+    AssertEmptyLarge(s1, Large1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, LargeString_AssignedWith_SmallStringView) {
+    string s1(Large1);
+    s1 = Small1.data();
+
+    AssertLarge(s1, Small1);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, LargeString_AssignedWith_SmallerLargeStringView) {
+    string s1(Large1);
+    s1 = Large2.data();
+
+    AssertLarge(s1, Large2);
+}
+
+TEST(StringAssignmentOperator_ConstPointer, LargeString_AssignedWith_LargerLargeStringView) {
+    string s1(Large2);
+    s1 = Large1.data();
+
+    AssertLarge(s1, Large1);
+}
--- test/src/common.hpp
+++ test/src/common.hpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include "string.hpp"
 
+constexpr string_view Empty = "";
 constexpr string_view Small1 = "small";
 constexpr string_view Small2 = "i am smol";
 constexpr string_view Large1 = "this is a large string";
