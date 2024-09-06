a0ee764c8ab58989289363a9edf6fc87745f2f44
Shlayne <1901.brc@gmail.com>
2024-09-06 16:17:21 -0700

    Rename some string constructor tests.

--- test/src/StringConstructors_Test.cpp
+++ test/src/StringConstructors_Test.cpp
@@ -38,41 +38,41 @@ TEST(StringConstructors, MoveConstructedWith_LargeString) {
     AssertLarge(s2, Large1);
 }
 
-TEST(StringConstructors, ConstructedWith_size_type_value_type_Small) {
+TEST(StringConstructors, ConstructedWith_SmallCountOfSameElement) {
     string s1(5, 'Q');
 
     AssertSmall(s1, Small1, false);
     ASSERT_STREQ(s1.data(), "QQQQQ");
 }
 
-TEST(StringConstructors, ConstructedWith_size_type_value_type_Large) {
+TEST(StringConstructors, ConstructedWith_LargeCountOfSameElement) {
     string s1(22, 'R');
 
     AssertLarge(s1, Large1, false);
     ASSERT_STREQ(s1.data(), "RRRRRRRRRRRRRRRRRRRRRR");
 }
 
-TEST(StringConstructors, ConstructedWith_string_view_type_Small) {
+TEST(StringConstructors, ConstructedWith_SmallStringView) {
     string_view sv1(Small1);
     string s1(sv1);
 
     AssertSmall(s1, Small1);
 }
 
-TEST(StringConstructors, ConstructedWith_string_view_type_Large) {
+TEST(StringConstructors, ConstructedWith_LargeStringView) {
     string_view sv1(Large1);
     string s1(sv1);
 
     AssertLarge(s1, Large1);
 }
 
-TEST(StringConstructors, ConstructedWith_const_pointer_Small) {
+TEST(StringConstructors, ConstructedWith_SmallConstPointer) {
     string s1(Small1);
 
     AssertSmall(s1, Small1);
 }
 
-TEST(StringConstructors, ConstructedWith_const_pointer_Large) {
+TEST(StringConstructors, ConstructedWith_LargeConstPointer) {
     string s1(Large1);
 
     AssertLarge(s1, Large1);
